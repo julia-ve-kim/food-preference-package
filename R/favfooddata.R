8e5e2e37ab36887add907e6d1b3c33ddc19490bb
Julia Kim <66211251+julia-ve-kim@users.noreply.github.com>
2024-02-07 19:19:28 -0500

    Add files via upload

---
+++ R/favfooddata.R
@@ -0,0 +1,25 @@
+#### Preamble ####
+# Purpose: Make an R package for a dataset about 10 people's favourite food.
+# Author: Julia Kim
+# Email: juliaym.kim@mail.utoronto.ca
+# Date: February 7th 2024
+# Prerequisites: Read Quiz 6b
+
+#### Workspace setup ####
+library(tidyverse)
+
+#### Make dataset ####
+set.seed(853) # for reproducibility
+
+food_data <- tibble(
+  name = c("Edward", "Helen", "Hugo", "Ian", "Monica",
+           "Myles", "Patricia", "Roger", "Rohan", "Ruth"),
+  fav_food = sample(
+    x = c("Pizza", "Sushi", "Burger", "Pasta", "Tacos",
+          "Salad", "Steak", "Curry", "Sandwich", "Soup"),
+    size = 10,
+    replace = TRUE
+  )
+)
+
+save(food_data, file = "data/food_data.rda")
